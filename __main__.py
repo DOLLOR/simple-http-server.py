13f1dea5fa21a0c7e062a705ee053644c4c7ed89
DOLLOR <8869507+DOLLOR@users.noreply.github.com>
2023-03-29 16:37:42 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -3,7 +3,11 @@
 import sys
 # import os
 # sys.path.append(os.getcwd())
-list(map(lambda x:print(x), sys.path))
+# list(map(lambda x:print(x), sys.path))
+print('')
+for pathItem in sys.path:
+    print(pathItem)
+print('')
 
 import sys
 print(['sys.dont_write_bytecode before',sys.dont_write_bytecode,])
